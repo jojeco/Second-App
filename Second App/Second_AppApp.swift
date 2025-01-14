2d1128e4fe4847b3b6dd850f027e6d51ea69716c
Jordan Coomber <jojeco717@gmail.com>
2025-01-14 15:06:07 -0700

    Initial Commit

---
+++ Second App/Second_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Second_AppApp.swift
+//  Second App
+//
+//  Created by Jordan Coomber on 2025-01-14.
+//
+
+import SwiftUI
+
+@main
+struct Second_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
